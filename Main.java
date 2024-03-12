b311524aaac358700e01bda0db76f1470289f618
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2024-03-12 13:55:01 +0300

    Описала логику класса Square

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle("krug1", 4);
+        Circle circle = new Circle("круг_1", 4);
         circle.calculateArea();
         circle.printAreaKrug();
 
         Rectangle rectangle = new Rectangle("прямоугольник_1", 2, 3);
         rectangle.printAreaRectangle();
         rectangle.printPerimeterRectangle();
-       
+
+        Square square = new Square("квадрат_1", 8);
+        square.printAreaRectangle();
+        square.printPerimeterRectangle();
     }
     
     
--- Square.java
+++ Square.java
@@ -1,3 +1,27 @@
-public class Square {
-    
+public class Square extends Rectangle {
+
+    public Square(String n, int l) {
+        super(n, l, 0);
+    }
+    public double calculateArea(){
+        double area = getLength() * getLength();
+        return area;
+    }
+
+    public double calculatePerimeter(){
+        double circumference = 4 * getLength();
+        return circumference;
+    }
+    public void printPerimeterRectangle() {
+        printPerimeter();
+        double circumference = calculatePerimeter();
+        System.out.println("составляет " + circumference);
+        System.out.println();
+    }
+    public void printAreaRectangle(){
+        printArea();
+        double area = calculateArea();
+        System.out.println("составляет " + area);
+        System.out.println();
+    }
 }
